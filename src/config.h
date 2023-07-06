56cacf4b48355ae72df105380e6ac87689d793d9
rolf <rolf@black.df9dq.de>
2023-07-06 21:25:57 +0200

    Clean up config structure
    For now the loader doesn't use the receiver configuration

--- src/config.h
+++ src/config.h
@@ -5,19 +5,39 @@
 #include "lpclib.h"
 
 __PACKED(struct _Config_t {
-    float referenceFrequency;
-    char nameBluetooth[44];
+    /* 0000  0x000 */
+    uint16_t version;
+    uint16_t serialNumber;
+
     uint16_t usbVID;
     uint16_t usbPID;
     uint16_t usbVERSION;
-    uint16_t __reserved36__;
-    wchar_t usbVendorString[48];
-    wchar_t usbProductString[32];
-    float rssiCorrectionHighGain;
-    float rssiCorrectionLowGain;
-    uint8_t __reservedD8__[32];
+    uint16_t __reserved036__;
+
+    /* 0012  0x00C */
+    float referenceFrequency;
+    float rssiCorrectionLnaOn;
+    float rssiCorrectionLnaOff;
+    float __reserved018__[25];
+    uint32_t baudrate;
+
+    /* 0128  0x080 */
+    char nameBluetooth[48];
+    wchar_t usbVendorString[64];
+    wchar_t usbProductString[64];
+    uint8_t __reserved1B0__[80];
+
+    /* 0512  0x200 */
+    uint16_t att_mtu;
+    uint16_t att_data_length;
+    uint16_t max_packet_length;
+    uint8_t __reserved206__[502];
+
+    uint32_t crc;
 });
 
-const struct _Config_t * CONFIG_getConfig (void);
+typedef struct _Config_t Config_t;
+
+extern const Config_t *config_g;
 
 #endif
