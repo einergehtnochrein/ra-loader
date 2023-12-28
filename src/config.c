6ecd1a4f41c1a6d0955bb1318db361181c29b72c
rolf <rolf@black.df9dq.de>
2023-12-28 05:54:15 +0100

    Remove unnecessary config defaults

--- src/config.c
+++ src/config.c
@@ -5,33 +5,9 @@
 #include "lpclib.h"
 #include "config.h"
 
-__SECTION(".config")
-static const struct _Config_t _factorySettingsDefault = {
-    .version = 3,
-    .baudrate = 115200.0f,
-
-    .usbVID = 0x169C,
-    .usbPID = 0x05DC,
-    .usbVERSION = 0x0100,
-
-    .referenceFrequency = 12.8e6,
-    .rssiCorrectionLnaOn = -13.0,
-    .rssiCorrectionLnaOff = 16.0,
-
-    .nameBluetooth = "",
-    .usbVendorString = L"leckasemmel.de/ra",
-    .usbProductString = L"Sondengott Ra",
-
-    .att_mtu = 23,
-    .att_data_length = 20,
-    .max_packet_length = 27,
-};
-
-__SECTION(".config")
-static volatile const Config_t _factorySettingsUser;
-
+extern uint32_t __CONFIG_ADDRESS__;
 
 /* Pointer to default configuration set at link time */
-const Config_t *config_g = &_factorySettingsDefault;
+const Config_t *config_g = (Config_t *)&__CONFIG_ADDRESS__;
 
 
